224d17eaf6223165f95459afaa811f4b64f48368
Osnova _ <compandtion@outlook.com>
2021-02-21 12:28:47 +0200

    Added definition for Resources functions

--- RescurseConsoleGame/Resource.cpp
+++ RescurseConsoleGame/Resource.cpp
@@ -1,1 +1,57 @@
 #include "Resource.h"
+
+uint64_t Resource::getLog() { return log; }
+uint64_t Resource::addLog(uint64_t count)
+{
+	return uint64_t();
+}
+uint64_t Resource::reduceLog(uint64_t count)
+{
+	return uint64_t();
+}
+void Resource::setLog()
+{
+}
+uint64_t Resource::getWood()
+{
+	return uint64_t();
+}
+uint64_t Resource::addWood(uint64_t count)
+{
+	return uint64_t();
+}
+uint64_t Resource::reduceWood(uint64_t count)
+{
+	return uint64_t();
+}
+void Resource::setWood()
+{
+}
+uint64_t Resource::getStone()
+{
+	return uint64_t();
+}
+uint64_t Resource::addstone(uint64_t count)
+{
+	return uint64_t();
+}
+uint64_t Resource::reduceStone(uint64_t count)
+{
+	return uint64_t();
+}
+void Resource::setStone()
+{
+}
+uint64_t Resource::addLog(uint64_t count);
+uint64_t Resource::reduceLog(uint64_t count);
+void Resource::setLog();
+
+uint64_t Resource::getWood();
+uint64_t Resource::addWood(uint64_t count);
+uint64_t Resource::reduceWood(uint64_t count);
+void Resource::setWood();
+
+uint64_t Resource::getStone();
+uint64_t Resource::addstone(uint64_t count);
+uint64_t Resource::reduceStone(uint64_t count);
+void Resource::setStone();
