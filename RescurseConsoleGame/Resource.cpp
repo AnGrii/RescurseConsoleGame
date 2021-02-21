ade14d2cfc43028ce2b549aa290822966a53eab4
Osnova _ <compandtion@outlook.com>
2021-02-21 12:35:17 +0200

    Added definition to methods

--- RescurseConsoleGame/Resource.cpp
+++ RescurseConsoleGame/Resource.cpp
@@ -3,55 +3,84 @@
 uint64_t Resource::getLog() { return log; }
 uint64_t Resource::addLog(uint64_t count)
 {
-	return uint64_t();
+	uint64_t limit = UINT64_MAX - log;
+
+	if ( limit > count ) {
+		log += count;
+	}
+	else {
+		log = UINT64_MAX;
+	}
 }
 uint64_t Resource::reduceLog(uint64_t count)
 {
-	return uint64_t();
+	if (log > count) {
+		log -= count;
+	}
+	else {
+		log = 0;
+	}
 }
-void Resource::setLog()
+void Resource::setLog(uint64_t count)
 {
+	log = count;
 }
+
 uint64_t Resource::getWood()
 {
-	return uint64_t();
+	return wood;
 }
 uint64_t Resource::addWood(uint64_t count)
 {
-	return uint64_t();
+	uint64_t limit = UINT64_MAX - wood;
+
+	if (limit > count) {
+		wood += count;
+	}
+	else {
+		wood = UINT64_MAX;
+	}
 }
 uint64_t Resource::reduceWood(uint64_t count)
 {
-	return uint64_t();
+	if (wood > count) {
+		wood -= count;
+	}
+	else {
+		wood = 0;
+	}
 }
-void Resource::setWood()
+void Resource::setWood(uint64_t count)
 {
+	wood = count;
 }
+
 uint64_t Resource::getStone()
 {
-	return uint64_t();
+	return stone;
 }
 uint64_t Resource::addstone(uint64_t count)
 {
-	return uint64_t();
+	uint64_t limit = UINT64_MAX - stone;
+
+	if (limit > count) {
+		stone += count;
+	}
+	else {
+		stone = UINT64_MAX;
+	}
 }
 uint64_t Resource::reduceStone(uint64_t count)
 {
-	return uint64_t();
+	if (stone > count) {
+		stone -= count;
+	}
+	else {
+		stone = 0;
+	}
 }
-void Resource::setStone()
+void Resource::setStone(uint64_t count)
 {
+	stone = count;
 }
-uint64_t Resource::addLog(uint64_t count);
-uint64_t Resource::reduceLog(uint64_t count);
-void Resource::setLog();
 
-uint64_t Resource::getWood();
-uint64_t Resource::addWood(uint64_t count);
-uint64_t Resource::reduceWood(uint64_t count);
-void Resource::setWood();
-
-uint64_t Resource::getStone();
-uint64_t Resource::addstone(uint64_t count);
-uint64_t Resource::reduceStone(uint64_t count);
-void Resource::setStone();
--- RescurseConsoleGame/Resource.h
+++ RescurseConsoleGame/Resource.h
@@ -22,17 +22,17 @@ public:
 	uint64_t getLog();
 	uint64_t addLog(uint64_t count);
 	uint64_t reduceLog(uint64_t count);
-	void setLog();
+	void setLog(uint64_t count);
 
 	uint64_t getWood();
 	uint64_t addWood(uint64_t count);
 	uint64_t reduceWood(uint64_t count);
-	void setWood();
+	void setWood(uint64_t count);
 
 	uint64_t getStone();
 	uint64_t addstone(uint64_t count);
 	uint64_t reduceStone(uint64_t count);
-	void setStone();
+	void setStone(uint64_t count);
 
 private:
 	uint64_t log, wood, stone;
