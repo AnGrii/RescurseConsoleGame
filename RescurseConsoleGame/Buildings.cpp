ec387ef1d2f351e1e70849cc6b6e267fecd5cf84
Osnova _ <compandtion@outlook.com>
2021-03-09 23:18:22 +0200

    Added param productivity to buildings

--- RescurseConsoleGame/Buildings.cpp
+++ RescurseConsoleGame/Buildings.cpp
@@ -3,15 +3,16 @@
 void BuildTemplate::Reinitialisate(std::vector<uint64_t> dataList)
 {
 	count = dataList[0];
-	inQueue = dataList[1];
-	moneyNeeded = dataList[2];
-	valueOfSkipTime = dataList[3];
-	endBuildTime = dataList[4];
-	buildTime = dataList[5];
-	resource1 = dataList[6];
-	resource2 = dataList[7];
-	resource3 = dataList[8];
-	activated = dataList[9];
+	productivity = dataList[1];
+	inQueue = dataList[2];
+	moneyNeeded = dataList[3];
+	valueOfSkipTime = dataList[4];
+	endBuildTime = dataList[5];
+	buildTime = dataList[6];
+	resource1 = dataList[7];
+	resource2 = dataList[8];
+	resource3 = dataList[9];
+	activated = dataList[10];
 }
 
 Buildings::Buildings(std::vector<std::vector<uint64_t>> dataList)
--- RescurseConsoleGame/Buildings.h
+++ RescurseConsoleGame/Buildings.h
@@ -9,12 +9,12 @@ class BuildTemplate
 {
 public:
 	BuildTemplate() :
-	count(0), inQueue(0), moneyNeeded(0), valueOfSkipTime(0), buildTime(0), endBuildTime(0), resource1(0), resource2(0), resource3(0), activated(0){}
+	count(0), productivity(0), inQueue(0), moneyNeeded(0), valueOfSkipTime(0), buildTime(0), endBuildTime(0), resource1(0), resource2(0), resource3(0), activated(0){}
 
 	void Reinitialisate(std::vector<uint64_t> dataList);
 	
 private:
-	uint64_t count, inQueue, moneyNeeded, valueOfSkipTime, buildTime, endBuildTime, resource1, resource2, resource3;
+	uint64_t count, productivity, inQueue, moneyNeeded, valueOfSkipTime, buildTime, endBuildTime, resource1, resource2, resource3;
 	bool activated;
 };
 
