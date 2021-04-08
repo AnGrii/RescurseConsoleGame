4a1b4fc9eb82bd85674fddc3d954bd2e81cefd64
Osnova _ <compandtion@outlook.com>
2021-04-08 21:22:18 +0300

    Added interface for upgrade building using

--- RescurseConsoleGame/ScienceCentre.cpp
+++ RescurseConsoleGame/ScienceCentre.cpp
@@ -1,1 +1,16 @@
 #include "ScienceCentre.h"
+
+void ScienceCentre::upgradeBuildings(BuildingsManager& buildings)
+{
+	ScienceCentre::upgradeBuildingsInfo(buildings);
+	ScienceCentre::upgradeBuildingsMenu(buildings);
+}
+
+void ScienceCentre::upgradeBuildingsInfo(const BuildingsManager& buildings)
+{
+
+}
+
+void ScienceCentre::upgradeBuildingsMenu(BuildingsManager& buildings)
+{
+}
--- RescurseConsoleGame/ScienceCentre.h
+++ RescurseConsoleGame/ScienceCentre.h
@@ -2,16 +2,20 @@
 #include <iostream>
 #include <vector>
 
+#include "BuildingsManager.h"
 
 class ScienceCentre
 {
 public:
+	static void upgradeBuildings(BuildingsManager& buildings);
+
+
 private:
 	ScienceCentre(){}
 
-	const double increaseValueCoefficient = 0.2;
+	const double increaseValueCoefficient = 1.2;
 
-
-
+	static void upgradeBuildingsInfo(const BuildingsManager& buildings);
+	static void upgradeBuildingsMenu(BuildingsManager& buildings);
 };
 
