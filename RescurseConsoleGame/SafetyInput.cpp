b430a5fd3a24b5329e03f1e461cdf887f5902e1d
Osnova _ <compandtion@outlook.com>
2021-04-24 12:10:53 +0300

    - Added methods to SafetyInput class

--- RescurseConsoleGame/SafetyInput.cpp
+++ RescurseConsoleGame/SafetyInput.cpp
@@ -1,1 +1,43 @@
 #include "SafetyInput.h"
+
+uint16_t SafetyInput::cinAndGetIDfromChar(std::string inputMSG, 
+	std::vector<char> nameIdLIst)
+{	
+	char input = '0';
+
+	std::cout << inputMSG;
+	std::cin >> input;
+	std::cout << std::endl;
+
+	uint16_t output = 0;
+	for (size_t i = 0; i < nameIdLIst.size(); i++)
+	{
+		if (nameIdLIst[i] == input) {
+			output = i;
+		}
+	}
+
+	return output;
+}
+
+uint64_t SafetyInput::cinAndGetNumFromInput(std::string inputMSG)
+{
+	std::string str_number = "0";
+	uint64_t number = 0;
+
+	std::cout << inputMSG;
+	std::cin >> str_number;
+	std::cout << std::endl;
+
+	// Deleting minus
+	if (str_number.size() >= 1) {
+		if (str_number[0] == '-') {
+			str_number.erase(str_number.begin());
+		}
+	}
+
+	number = std::stoull(str_number);
+
+	return number;
+}
+
--- RescurseConsoleGame/SafetyInput.h
+++ RescurseConsoleGame/SafetyInput.h
@@ -1,11 +1,15 @@
 #pragma once
 #include <string>
 #include <iostream>
+#include <vector>
 
 class SafetyInput
 {
 public:
-private:
+	uint16_t cinAndGetIDfromChar(std::string inputMSG, 
+		std::vector<char> nameIdLIst);
 
+	uint64_t cinAndGetNumFromInput(std::string inputMSG);
+private:
 };
 
